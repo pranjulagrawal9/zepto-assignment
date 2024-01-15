ec32705e41b5890a3be1563a862081e06fdfd6d6
Pranjul Agrawal <pranjulagrawal9@gmail.com>
2024-01-15 23:23:01 +0530

    Generate fake users data

---
+++ src/data/users.js
@@ -0,0 +1,32 @@
+export const user = [
+  {
+    id: 1,
+    name: "Isabella Gomez",
+    imageUrl: "https://randomuser.me/api/portraits/women/81.jpg",
+    email: "isabella.gomez@example.com",
+  },
+  {
+    id: 2,
+    name: "Holly Morrison",
+    imageUrl: "https://randomuser.me/api/portraits/women/55.jpg",
+    email: "holly.morrison@example.com",
+  },
+  {
+    id: 3,
+    name: "Nathan Peterson",
+    imageUrl: "https://randomuser.me/api/portraits/men/45.jpg",
+    email: "nathan.peterson@example.com",
+  },
+  {
+    id: 4,
+    name: "Jesus Wagner",
+    imageUrl: "https://randomuser.me/api/portraits/men/74.jpg",
+    email: "jesus.wagner@example.com",
+  },
+  {
+    id: 5,
+    name: "Danny Hughes",
+    imageUrl: "https://randomuser.me/api/portraits/men/98.jpg",
+    email: "danny.hughes@example.com",
+  },
+];
